3bb6efcf783ece508b67e957f564f07499863e19
adachshenko <adachshenko@gmail.com>
2019-03-27 11:09:34 +0600

    Task 2

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Top from './components/top';
+import Middle from './components/middle';
+import Bottom from './components/bottom';
+
+import "index.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Top />
+      <Middle />
+      {/* <Bottom /> */}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ client/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const getBaseConfig = env => ({
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  mode: (env && env.NODE_ENV) || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          env && env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+});
+
+const prodConfig = {
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    },
+    minimizer: [new UglifyJsPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new OptimizeCssAssetsPlugin(),
+    new Visualizer({ filename: './statistics.html' })
+  ],
+};
+
+module.exports = env => env && env.NODE_ENV === 'production' ? merge(getBaseConfig(env), prodConfig) : getBaseConfig(env);
